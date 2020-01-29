13211937b89d95f95ce83b3415513f39503613d9
jerrygacket <jerrygacket@gmail.com>
2020-01-29 12:56:16 +0300

    ОТключил экшен генератора ролей

--- controllers/RbacController.php
+++ controllers/RbacController.php
@@ -9,9 +9,9 @@ use yii\web\Controller;
 
 class RbacController extends Controller
 {
-    public function actionGen(){
-        /** @var RbacComponent $rbac */
-        $rbac=\Yii::$app->rbac;
-        $rbac->generateRbac();
-    }
+//    public function actionGen(){
+//        /** @var RbacComponent $rbac */
+//        $rbac=\Yii::$app->rbac;
+//        $rbac->generateRbac();
+//    }
 }
